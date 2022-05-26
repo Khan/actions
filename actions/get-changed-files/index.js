7978f5f923c68697ae3abbc87ffceca093559452
Jared Forsyth <jared@jaredforsyth.com>
2022-05-26 12:15:40 -0500

    Bring "@jaredly/get-changed-files" into our actions monorepo, stripping it down to only what we need (#11)
    ## Summary:
    We've unified around json encoding to deal with spaces, and we only ever ask for added_modified and renamed, so I merged them together.
    
    This is my first time trying `actions/github-script` with a local require, and it turns out it's relative to the repo base (which makes sense). Because of this, I had to change `build.mjs` to turn `require('./actions/get-changed-files/index.js')` into `require('./index.js')` when publishing these actions individually.
    
    Issue: XXX-XXXX
    
    ## Test plan:
    See the check on this one!
    
    <img width="1488" alt="image" src="https://user-images.githubusercontent.com/112170/170351762-249bec46-539c-4c2e-a905-7547ff916a4e.png">
    
    Author: jaredly
    
    Reviewers: somewhatabstract
    
    Required Reviewers:
    
    Approved By: somewhatabstract, somewhatabstract
    
    Checks: ✅ gerald, ✅ Lints
    
    Pull Request URL: https://github.com/Khan/actions/pull/11

---
+++ actions/get-changed-files/index.js
@@ -0,0 +1,73 @@
+// Get the changed files for a pull-request or push
+
+const getBaseAndHead = (context, core) => {
+    switch (context.eventName) {
+        case "pull_request_target":
+        case "pull_request":
+            return [
+                context.payload.pull_request?.base?.ref,
+                context.payload.pull_request?.head?.sha,
+            ];
+        case "push":
+            return [context.payload.before, context.payload.after];
+        default:
+            core.setFailed(
+                `This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
+                    "Please submit an issue on this action's GitHub repo if you believe this in correct.",
+            );
+    }
+};
+
+module.exports = async ({github, context, core}) => {
+    const absolute = !!core.getInput("absolute", {required: false});
+
+    const [base, head] = getBaseAndHead(context, core);
+    core.info(`Base: ${base}\nHead: ${head}`);
+
+    // Ensure that the base and head properties are set on the payload.
+    if (!base || !head) {
+        core.setFailed(
+            `The base and head commits are missing from the payload for this ${context.eventName} event.`,
+        );
+        return;
+    }
+
+    // Use GitHub's compare two commits API.
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    const response = await github.rest.repos.compareCommits({
+        base,
+        head,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    });
+
+    // Ensure that the request was successful.
+    if (response.status !== 200) {
+        core.setFailed(
+            `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200.`,
+        );
+    }
+
+    const files = response.data.files.filter((file) =>
+        ["added", "modified", "renamed"].includes(file.status),
+    );
+    const fileNames = files.map((file) =>
+        absolute
+            ? join(process.env.GITHUB_WORKSPACE, file.filename)
+            : file.filename,
+    );
+    const serialized = JSON.stringify(fileNames);
+
+    core.info(`Added or renamed or modified: ${serialized}`);
+    core.setOutput("files", serialized);
+};
+
+const join = (base, name) => {
+    if (!base || !base.length) {
+        return name;
+    }
+    if (!base.endsWith("/")) {
+        base += "/";
+    }
+    return base + name;
+};
