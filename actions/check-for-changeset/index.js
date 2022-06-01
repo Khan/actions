b3eba8d5622a1799a485a1400637467eb96063b2
Jared Forsyth <jared@jaredforsyth.com>
2022-06-01 10:59:45 -0500

    allow filtering to be configurable

--- actions/check-for-changeset/index.js
+++ actions/check-for-changeset/index.js
@@ -1,14 +1,12 @@
 module.exports = ({context, core, inputFiles}) => {
+    if (!inputFiles.length) {
+        return; // no relevant files changed, ignore
+    }
     core.debug("Changed files: " + inputFiles);
-    const branchName = context.payload.pull_request.head.ref;
-    console.log(branchName);
 
+    const branchName = context.payload.pull_request.head.ref;
     if (branchName === "changeset-release/main") {
         return; // release PRs don't need changesets.
-    }
-
-    if (inputFiles.every((name) => name.startsWith(".github/"))) {
-        return; // changes to workflows don't require changesets
     }
 
     const hasChangeset = inputFiles.some((name) => {
