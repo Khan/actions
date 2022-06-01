c8106cdd24824f38a3934dab74b8000ca16f259a
Jared Forsyth <jared@jaredforsyth.com>
2022-06-01 11:18:01 -0500

    Exclude the changeset-release/main branch from the changeset check, and .github/-only changes (#17)
    ## Summary:
    To require the --empty a little less often.
    
    Issue: XXX-XXXX
    
    ## Test plan:
    See this check to verify that it's still working for normal PRs.
    
    Author: jaredly
    
    Reviewers: jeresig, jaredly
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ gerald, ✅ Lints
    
    Pull Request URL: https://github.com/Khan/actions/pull/17

---
+++ actions/check-for-changeset/index.js
@@ -0,0 +1,23 @@
+module.exports = ({context, core, inputFiles}) => {
+    if (!inputFiles.length) {
+        return; // no relevant files changed, ignore
+    }
+    core.debug("Changed files: " + inputFiles);
+
+    const branchName = context.payload.pull_request.head.ref;
+    if (branchName === "changeset-release/main") {
+        return; // release PRs don't need changesets.
+    }
+
+    const hasChangeset = inputFiles.some((name) => {
+        return /^\.changeset\/.*\.md/.test(name);
+    });
+    if (!hasChangeset) {
+        core.setFailed(
+            "This PR does not have a changeset. You can add one by " +
+                "running `yarn changeset` and following the prompts.\n" +
+                "If this PR doesn't need a changeset, run `yarn changeset " +
+                "--empty` and commit results.",
+        );
+    }
+};
