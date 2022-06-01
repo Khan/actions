561b89ccf60af38be346ac36e665f6755c94e929
Jared Forsyth <jared@jaredforsyth.com>
2022-06-01 11:46:31 -0500

    impl

---
+++ actions/filter-files/index.js
@@ -0,0 +1,27 @@
+/**
+ * Parse a list, that could be separated by commas or newlines.
+ */
+const parseList = (raw) => {
+    if (!raw.trim()) {
+        return [];
+    }
+    if (raw.includes(",")) {
+        return raw.split(",").map((item) => item.trim());
+    }
+    return raw.split("\n").map((item) => item.trim());
+};
+
+module.exports = ({extensionsRaw, exactFilesRaw, inputFiles, invert}) => {
+    const extensions = parseList(extensionsRaw);
+    const exactFiles = parseList(exactFilesRaw);
+    const directories = exactFiles.filter((name) => name.endsWith("/"));
+    const result = inputFiles.filter((name) => {
+        const matched =
+            extensions.some((ext) => name.endsWith(ext)) ||
+            exactFiles.includes(name) ||
+            directories.some((dir) => name.startsWith(dir));
+        return matched === !invert;
+    });
+    console.log(`Filtered Files: ${JSON.stringify(result)}`);
+    return result;
+};
