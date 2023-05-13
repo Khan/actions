a2154dbc4a2068b79ac6da8f68e55f9b4dd26383
Jeremy Wiebe <jeremy@khanacademy.org>
2023-05-13 08:57:04 -0700

    Tweaks for PR review

--- actions/filter-files/index.js
+++ actions/filter-files/index.js
@@ -23,14 +23,14 @@ module.exports = ({
 }) => {
     const extensions = parseList(extensionsRaw);
     const exactFiles = parseList(exactFilesRaw);
-    const globMather = picomatch(parseList(globsRaw));
+    const globMatcher = picomatch(parseList(globsRaw));
     const directories = exactFiles.filter((name) => name.endsWith("/"));
     const result = inputFiles.filter((name) => {
         const matched =
             extensions.some((ext) => name.endsWith(ext)) ||
             exactFiles.includes(name) ||
             directories.some((dir) => name.startsWith(dir)) ||
-            globMather(name);
+            globMatcher(name);
         return matched === !invert;
     });
     core.info(`Filtered Files: ${JSON.stringify(result)}`);
