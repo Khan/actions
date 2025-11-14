8df06a1276ce97c99a4e7a3d9bbd4277cf4f22e4
Jeremy Wiebe <jeremy@khanacademy.org>
2025-11-13 16:38:59 -0800

    Debugging

--- actions/get-changed-files/index.js
+++ actions/get-changed-files/index.js
@@ -9,19 +9,9 @@ const getBaseAndHead = async (github, context, core) => {
                 context.payload.pull_request?.head?.sha,
             ];
         case "push":
-            // eslint-disable-next-line no-console
-            console.log(
-                `Before: ${JSON.stringify(
-                    context.payload,
-                    undefined,
-                    2,
-                )} (SHA: ${context.payload.before})`,
-            );
-
-            // For push events, if this is a new branch, the base_ref will be
-            // null and before will be all zeros (courtesy
-            // https://stackoverflow.com/a/61861763)
-            if (context.payload.base_ref != null) {
+            // For push events, if this is a new branch, the before may be all
+            // zeros (courtesy https://stackoverflow.com/a/61861763).
+            if (context.payload.before?.replaceAll("0", "").length > 0) {
                 return [context.payload.before, context.payload.after];
             } else {
                 // If we're on a new branch, then we try to find an open PR
