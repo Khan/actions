cc925538d481d2f5c77f61e20027a0ba6c2d869e
Kevin Barabash <kevinbarabash@khanacademy.org>
2026-03-09 11:52:51 -0400

    [fix-run-publish] Fix issues with utils/run-publish.ts

--- utils/run-publish.ts
+++ utils/run-publish.ts
@@ -2,18 +2,20 @@
  * This script runs whenever a change lands to the 'main' branch,
  * publishing any new versions of actions that are needed.
  */
-import fs from "fs";
+import * as fs from "fs";
 import {publishAsNeeded} from "./publish.ts";
 
-const args = process.argv.slice(2);
-const dryRun = args.includes("--dry-run");
-const packageNames = fs
-    .readdirSync("actions")
-    .filter((name) => fs.statSync(`actions/${name}`).isDirectory());
+async function main() {
+    const args = process.argv.slice(2);
+    const dryRun = args.includes("--dry-run");
+    const packageNames = fs
+        .readdirSync("actions")
+        .filter((name) => fs.statSync(`actions/${name}`).isDirectory());
 
-try {
     await publishAsNeeded(packageNames, dryRun);
-} catch (err) {
+}
+
+main().catch((err) => {
     console.error(err);
     process.exit(1);
-}
+});
